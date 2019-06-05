72f60b89711bfe31eaf59bf82754f31cc979722a
sabeywardhana <sabeywardhana@mitrai.com>
2019-06-05 22:20:22 +0530

    init commit

---
+++ create-dbs-users.sql
@@ -0,0 +1,15 @@
+create database WSO2_USER_DB;
+use WSO2_USER_DB;
+grant all on WSO2_USER_DB.* TO regadmin@"dev-mysql.demo.com" identified by "regadmin";
+ 
+create database REGISTRY_DB;
+use REGISTRY_DB;
+grant all on REGISTRY_DB.* TO regadmin@"dev-mysql.demo.com" identified by "regadmin";
+ 
+create database REGISTRY_LOCAL1;
+use REGISTRY_LOCAL1;
+grant all on REGISTRY_LOCAL1.* TO regadmin@"dev-mysql.demo.com" identified by "regadmin";
+  
+create database REGISTRY_LOCAL2;
+use REGISTRY_LOCAL2;
+grant all on REGISTRY_LOCAL2.* TO regadmin@"dev-mysql.demo.com" identified by "regadmin";
