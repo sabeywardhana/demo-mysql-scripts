96398b9430647b6e5d6b21cd286a95c0360dfad3
sabeywardhana <sabeywardhana@mitrai.com>
2019-06-07 01:29:13 +0530

    Local reg DBs

--- create-dbs-users.sql
+++ create-dbs-users.sql
@@ -1,15 +1,20 @@
+CREATE USER 'regadmin'@'%' IDENTIFIED BY 'regadmin';
+
 create database WSO2_USER_DB;
 use WSO2_USER_DB;
-grant all on WSO2_USER_DB.* TO regadmin@"%.demo.com" identified by "regadmin";
+grant all on WSO2_USER_DB.* TO 'regadmin'@'%' identified by 'regadmin';
  
 create database REGISTRY_DB;
 use REGISTRY_DB;
-grant all on REGISTRY_DB.* TO regadmin@"%.demo.com" identified by "regadmin";
+grant all on REGISTRY_DB.* TO 'regadmin'@'%' identified by 'regadmin';
  
 create database REGISTRY_LOCAL1;
 use REGISTRY_LOCAL1;
-grant all on REGISTRY_LOCAL1.* TO regadmin@"%.demo.com" identified by "regadmin";
-  
+grant all on REGISTRY_LOCAL1.* TO 'regadmin'@'%' identified by 'regadmin';
+
 create database REGISTRY_LOCAL2;
 use REGISTRY_LOCAL2;
-grant all on REGISTRY_LOCAL2.* TO regadmin@"%.demo.com" identified by "regadmin";
+grant all on REGISTRY_LOCAL2.* TO 'regadmin'@'%' identified by 'regadmin';
+
+
+	
